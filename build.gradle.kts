a7b2fe03ef4bcdc064708c34de2b3a0ab7fc7169
shabinder <dev.shabinder@gmail.com>
2023-04-30 08:56:29 +0530

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.accessors.dm.LibrariesForDeps
+
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
@@ -14,11 +16,23 @@ repositories {
 }
 
 dependencies {
-    compileOnly(deps.kotlin.kotlinGradlePlugin)
-    compileOnly(deps.androidx.gradle.plugin)
+    with(deps) {
+        implementation(androidx.gradle.plugin)
+        implementation(kotlin.compose.gradle)
+        implementation(ktlint.gradle)
+        implementation(mosaic.gradle)
+        implementation(kotlin.kotlinGradlePlugin)
+        implementation(sqldelight.gradle.plugin)
+        implementation(i18n4k.gradle.plugin)
+        implementation(kotlin.serialization)
+        implementation(google.services)
+        implementation(firebase.plugins.crashlytics)
+    }
+
     compileOnly("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.15.0")
     compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator::0.13.2")
     compileOnly("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
+    compileOnly(files(deps.javaClass.superclass.protectionDomain.codeSource.location))
     testImplementation(kotlin("test"))
 }
 
---
+++ src/main/java/com/arkivanov/gradle/SetupRepositories.kt
@@ -0,0 +1,22 @@
+package com.arkivanov.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.kotlin.dsl.maven
+import org.gradle.kotlin.dsl.repositories
+
+fun Project.setupRepositories() = repositories { setupRepos() }
+fun Project.setupRepositoriesInAll() = allprojects { repositories { setupRepos() } }
+
+fun RepositoryHandler.setupRepos() {
+    // local built system
+    mavenLocal()
+
+    // remote repositories
+    google()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+    maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+    maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
