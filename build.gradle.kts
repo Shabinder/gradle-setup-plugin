7c62deaf08b13dbb050ea5ee5126e203b3263e88
shabinder <dev.shabinder@gmail.com>
2023-05-01 01:05:44 +0530

    Revert "Web-App WIP"
    This reverts commit 43261134c9e2109fc762bb5d66e6dbd38dbfcd4e.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
         implementation(i18n4k.gradle.plugin)
         implementation(kotlin.serialization)
         implementation(google.services)
-        implementation(google.services)
         implementation(firebase.plugins.crashlytics)
     }
     compileOnly("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.3.1")
