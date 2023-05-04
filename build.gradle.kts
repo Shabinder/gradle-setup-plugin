6ae5e5da9044f81cd789824e541dc04e081fea0d
shabinder <dev.shabinder@gmail.com>
2023-05-04 09:53:12 +0530

    Intervention Logic Setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.accessors.dm.LibrariesForDeps
-
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
@@ -15,6 +13,14 @@ repositories {
     google()
 }
 
+kotlin {
+    // Add Deps to compilation, so it will become available in main project
+    sourceSets.getByName("main").kotlin.srcDir(
+        file("$buildDir/generated/source/buildConfig/main")
+    )
+}
+
+
 dependencies {
     with(deps) {
         implementation(androidx.gradle.plugin)
@@ -28,6 +34,7 @@ dependencies {
         implementation(google.services)
         implementation(firebase.plugins.crashlytics)
     }
+    implementation("com.github.gmazzo.buildconfig:plugin:4.0.4")
     compileOnly("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.3.1")
     compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator:0.12.1")
     compileOnly("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
---
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -0,0 +1,38 @@
+package com.arkivanov.gradle
+
+import com.github.gmazzo.gradle.plugins.BuildConfigExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import kotlin.reflect.full.memberProperties
+
+object SoundBoundConfig {
+    const val APP_VERSION: String = "0.0.1"
+    const val APP_VERSION_CODE: Long = 1L
+}
+
+fun Project.setupBuildConfig(
+    configure: BuildConfigExtension.() -> Unit = {}
+) {
+    plugins.apply("com.github.gmazzo.buildconfig")
+    extensions.configure<BuildConfigExtension> {
+        className("SoundBoundConfig")   // forces the class name. Defaults to 'BuildConfig'
+        packageName("in.shabinder.soundbound")
+        useKotlinOutput {
+            internalVisibility = false
+        }
+        SoundBoundConfig::class.memberProperties.forEach {
+            when {
+                it.name.equals("INSTANCE") -> return@forEach
+                it.returnType.toString() == "kotlin.Long" -> buildConfigField(it.returnType.toString(), it.name, "${it.call()}L")
+                it.returnType.toString().substringAfterLast(".").equals("String") -> buildConfigField(
+                    it.returnType.toString(),
+                    it.name,
+                    "\"${it.call()}\""
+                )
+
+                else -> buildConfigField(it.returnType.toString(), it.name, it.call().toString())
+            }
+        }
+        configure()
+    }
+}
