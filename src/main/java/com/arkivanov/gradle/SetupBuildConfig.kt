eedb6814bb86086f438e5e023240ae116fc719d8
shabinder <shabinder.singh@theatom.app>
2023-05-28 08:09:24 +0530

    HomeScreen GridRow and Cleanup

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -6,8 +6,8 @@ import org.gradle.kotlin.dsl.configure
 import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
-    const val APP_VERSION: String = "0.0.1"
-    const val APP_VERSION_CODE: Long = 1L
+    const val APP_VERSION: String = "0.0.2"
+    const val APP_VERSION_CODE: Long = 2L
 }
 
 fun Project.setupBuildConfig(
