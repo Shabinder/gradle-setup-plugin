2c5b7164c8e72fdf345bcac5b143ea71b2331dde
shabinder <shabinder.singh@theatom.app>
2023-07-16 18:36:07 +0530

    Version Bump, toast for Source Update

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
     const val APP_VERSION: String = "0.0.3"
-    const val APP_VERSION_CODE: Long = 3L
+    const val APP_VERSION_CODE: Long = 4L
 }
 
 fun Project.setupBuildConfig(
