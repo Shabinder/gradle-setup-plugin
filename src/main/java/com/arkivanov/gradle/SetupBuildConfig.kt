b13a0f33385408552a24b2899e49f56e8f4b49ff
shabinder <shabinder.singh@theatom.app>
2023-07-27 15:30:06 +0530

    Download Location Fix and Version Bump

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
     const val APP_VERSION: String = "0.0.4"
-    const val APP_VERSION_CODE: Long = 5L
+    const val APP_VERSION_CODE: Long = 7L
 }
 
 fun Project.setupBuildConfig(
