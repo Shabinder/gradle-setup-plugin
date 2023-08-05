683f81ffce0109ac4f2e96e27a2000fc40728805
shabinder <shabinder.singh@theatom.app>
2023-08-05 22:59:26 +0530

    Error Detail Fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ kotlin {
     )
 }
 
-
 dependencies {
     with(deps) {
         implementation(androidx.gradle.plugin)
@@ -33,13 +32,15 @@ dependencies {
         implementation(kotlin.serialization)
         implementation(google.services)
         implementation(firebase.plugins.crashlytics)
+        implementation(zipline.gradlePlugin)
+        compileOnly(zipline.loader)
     }
     implementation("com.github.gmazzo.buildconfig:plugin:4.0.4")
     compileOnly("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.3.1")
-    compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator:0.12.1")
+    compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
     compileOnly("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
     compileOnly(files(deps.javaClass.superclass.protectionDomain.codeSource.location))
-    implementation("androidx.benchmark:benchmark-baseline-profile-gradle-plugin:1.2.0-alpha16")
+    implementation("androidx.benchmark:benchmark-baseline-profile-gradle-plugin:1.2.0-beta02")
     testImplementation(kotlin("test"))
 }
 
