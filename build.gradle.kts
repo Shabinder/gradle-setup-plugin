13ed80d496900d1d728ef499d807831e6c78d9a5
shabinder <shabinder.singh@theatom.app>
2023-08-09 03:53:55 +0530

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
         implementation(zipline.gradlePlugin)
         compileOnly(zipline.loader)
     }
+//    implementation("com.guardsquare:proguard-gradle:7.3.2")
     implementation("com.github.gmazzo.buildconfig:plugin:4.0.4")
     compileOnly("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.3.1")
     compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
