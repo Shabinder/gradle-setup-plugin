463218b06722663c7840759bbae01b0120010bdf
shabinder <shabinder.singh@theatom.app>
2023-08-15 10:41:50 +0530

    Version Bump to 0.8

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -6,8 +6,8 @@ import org.gradle.kotlin.dsl.configure
 import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
-    const val APP_VERSION: String = "0.0.6"
-    const val APP_VERSION_CODE: Long = 9L
+    const val APP_VERSION: String = "0.0.8"
+    const val APP_VERSION_CODE: Long = 11L
 }
 
 fun Project.setupBuildConfig(
