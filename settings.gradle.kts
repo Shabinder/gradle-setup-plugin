88a841df9c78bb0cb8968b8356163b432364dbf2
Shabinder Singh <dev.shabinder@gmail.com>
2023-10-30 01:48:18 +0530

    WIP: Auth Impl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("deps") {
-            from("in.shabinder:soundbound-extensions-catalog:0.9")
+            from("in.shabinder:soundbound-extensions-catalog:0.10")
             library("google-services", "com.google.gms:google-services:4.3.15")
             library("firebase-bom", "com.google.firebase:firebase-bom:31.5.0")
             library("firebase-plugins-crashlytics", "com.google.firebase:firebase-crashlytics-gradle:2.9.5")
