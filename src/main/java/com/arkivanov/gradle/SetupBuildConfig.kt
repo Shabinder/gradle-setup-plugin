223ee49e71666a837d1a87f39f506f4f94e3590c
Shabinder Singh <dev.shabinder@gmail.com>
2023-11-21 01:26:03 +0530

    HomeLoad Fix

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -6,8 +6,8 @@ import org.gradle.kotlin.dsl.configure
 import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
-    const val APP_VERSION: String = "0.0.15"
-    const val APP_VERSION_CODE: Long = 20L
+    const val APP_VERSION: String = "0.0.16"
+    const val APP_VERSION_CODE: Long = 21L
 }
 
 fun Project.setupBuildConfig(
