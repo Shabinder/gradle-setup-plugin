f2ab4b8ee3479b727c8be47362c73bb472858ffa
Shabinder Singh <dev.shabinder@gmail.com>
2023-12-01 21:20:36 +0530

    0.28 Notification Dialog.

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -6,8 +6,8 @@ import org.gradle.kotlin.dsl.configure
 import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
-    const val APP_VERSION: String = "0.0.21"
-    const val APP_VERSION_CODE: Long = 26L
+    const val APP_VERSION: String = "0.0.23"
+    const val APP_VERSION_CODE: Long = 28L
 }
 
 fun Project.setupBuildConfig(
