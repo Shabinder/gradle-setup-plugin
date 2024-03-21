72b85f6263ff51304cab419b70e5c00d99c7f1d5
Shabinder Singh <dev.shabinder@gmail.com>
2024-03-21 06:39:32 +0530

    Cleanup and use external zipline-loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,16 +33,14 @@ dependencies {
         implementation(kotlin.serialization)
         implementation(google.services)
         implementation(firebase.plugins.crashlytics)
-        implementation(zipline.gradlePlugin)
-        compileOnly(zipline.loader)
     }
 //    implementation("com.guardsquare:proguard-gradle:7.3.2")
     implementation("com.github.gmazzo.buildconfig:plugin:4.0.4")
     compileOnly("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.15.0")
-    compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
+    compileOnly("org.jetbrains.kotlinx:binary-compatibility-validator:0.14.0")
     compileOnly("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
     compileOnly(files(deps.javaClass.superclass.protectionDomain.codeSource.location))
-    implementation("androidx.benchmark:benchmark-baseline-profile-gradle-plugin:1.2.1")
+    implementation("androidx.benchmark:benchmark-baseline-profile-gradle-plugin:1.2.3")
     testImplementation(kotlin("test"))
 }
 
