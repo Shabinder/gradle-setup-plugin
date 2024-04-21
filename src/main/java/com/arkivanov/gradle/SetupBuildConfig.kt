efed22a5e95059c1f3ba728690db8df39fb608b9
Shabinder Singh <dev.shabinder@gmail.com>
2024-04-21 11:24:25 +0530

    Scripts to Build VLC.
     - Source: https://github.com/simplex-chat/simplex-chat/tree/stable/scripts/desktop

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -6,8 +6,8 @@ import org.gradle.kotlin.dsl.configure
 import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
-    const val APP_VERSION: String = "0.0.25"
-    const val APP_VERSION_CODE: Long = 30L
+    const val APP_VERSION: String = "0.0.26"
+    const val APP_VERSION_CODE: Long = 31L
 }
 
 fun Project.setupBuildConfig(
