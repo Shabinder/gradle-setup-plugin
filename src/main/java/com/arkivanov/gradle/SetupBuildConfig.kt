ff1717cf3c4bb1784ea703e0e1a112d685d7479f
Shabinder Singh <dev.shabinder@gmail.com>
2024-05-04 03:47:49 +0530

    Version Bump

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -6,8 +6,8 @@ import org.gradle.kotlin.dsl.configure
 import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
-    const val APP_VERSION: String = "0.0.26"
-    const val APP_VERSION_CODE: Long = 31L
+    const val APP_VERSION: String = "0.0.27"
+    const val APP_VERSION_CODE: Long = 32L
 }
 
 fun Project.setupBuildConfig(
