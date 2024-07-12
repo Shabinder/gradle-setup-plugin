3fc6e098586f78121e2615d36161eac3ea4aac4f
Shabinder Singh <dev.shabinder@gmail.com>
2024-07-13 00:21:23 +0530

    Remote Config impl

--- src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
+++ src/main/java/com/arkivanov/gradle/SetupBuildConfig.kt
@@ -6,8 +6,8 @@ import org.gradle.kotlin.dsl.configure
 import kotlin.reflect.full.memberProperties
 
 object SoundBoundConfig {
-    const val APP_VERSION: String = "0.0.29"
-    const val APP_VERSION_CODE: Long = 37L
+    const val APP_VERSION: String = "0.0.30"
+    const val APP_VERSION_CODE: Long = 38L
 }
 
 fun Project.setupBuildConfig(
