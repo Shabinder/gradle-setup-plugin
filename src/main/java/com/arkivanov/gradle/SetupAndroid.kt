2c104a1cbbb8a9c9b2640b57eb5a4731aa154cd9
Shabinder Singh <contact@shabinder.in>
2024-09-30 22:36:09 +0530

    WIP: Cleanup

--- src/main/java/com/arkivanov/gradle/SetupAndroid.kt
+++ src/main/java/com/arkivanov/gradle/SetupAndroid.kt
@@ -8,7 +8,6 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.withGroovyBuilder
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
